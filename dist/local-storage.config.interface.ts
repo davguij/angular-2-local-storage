45f9311e5f60ee074127babb13cebe3947d4de05
David Guijarro <guijarro.dav@gmail.com>
2017-01-20 13:31:55 +0100

    Changed dist folder for installing from GH.

--- dist/local-storage.config.interface.ts
+++ dist/local-storage.config.interface.ts
@@ -1,5 +1,7 @@
 import { INotifyOptions } from './notify-options.interface';
+
 export interface ILocalStorageServiceConfig {
+    // Properties:
     notifyOptions?: INotifyOptions;
     prefix?: string;
     storageType?: 'sessionStorage' | 'localStorage';
